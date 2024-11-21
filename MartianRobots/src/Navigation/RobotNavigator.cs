14fa3bfa7480b392c2a32c0c40f4faec186a6f2f
Jake <55953689+jakecreely@users.noreply.github.com>
2024-11-21 12:06:32 +0000

    Robot Navigation

---
+++ MartianRobots/src/Navigation/RobotNavigator.cs
@@ -0,0 +1,35 @@
+public class RobotNavigator
+{
+    public void Navigate(Grid grid, Robot robot, string instructions)
+    {
+        
+        Position currentPosition = robot.CurrentPosition;
+        bool isLost = false;
+
+        foreach (char instruction in instructions)
+        {
+            Position newPosition = instruction switch
+            {
+                'L' or 'R' => currentPosition.Turn(instruction),
+                'F' => currentPosition.MoveForward(),
+                _ => throw new ArgumentException("Invalid instruction")
+            };
+            
+            // if newPosition is out of the grid bounds
+            // if has already been lost by robot - skip - dont assign position to robot
+            // has not been lost then robot is lost and navigation ends
+            // if it is valid position then update the robots position with the current one
+
+            if (grid.IsOutOfBounds(newPosition) && !isLost)
+            {
+                if (grid.HasAlreadyLostFromPosition(newPosition)) continue;
+                isLost = true;
+                grid.AddLostPosition(newPosition);
+            }
+            else
+            {
+                robot.CurrentPosition = newPosition;
+            }
+        }
+    }
+}
