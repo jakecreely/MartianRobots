a6c7190aa6c764701e796ae70dcfd50185151f6f
Jake <55953689+jakecreely@users.noreply.github.com>
2024-11-21 13:58:36 +0000

    Break For Lunch - Next Task Console Input/Output

--- MartianRobots/Program.cs
+++ MartianRobots/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Read the grid input
+// Create the grid
+// Read robot one starting position
+// Create robot
+// Read instructions
+// Add robot and instructions to an array
+// Repeat until user stops
+// Run navigate on robot in sequential order
