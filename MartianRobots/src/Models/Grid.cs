d6eff3e93f94f0754fce2f6e16acf74e2f5f4462
Jake <55953689+jakecreely@users.noreply.github.com>
2024-11-21 12:05:57 +0000

    Updated Grid functionality and maximum size

--- MartianRobots/src/Models/Grid.cs
+++ MartianRobots/src/Models/Grid.cs
@@ -7,17 +7,22 @@ public class Grid
     public Grid(int width, int height)
     {
         // Minimum size of grid is 1 by 1 and max is 50 by 50
-        if (width < 1 || height < 1 || width > 50 || height > 50)
+        if (width < 1 || height < 1 || width > 51 || height > 51)
         {
-            throw new ArgumentException("Grid coordinates must be between 1 and 50");
+            throw new ArgumentException("Width and height must be between 1 and 51");
         }
         Width = width;
         Height = height;
         LostPositions = new List<Position>();
     }
 
+    public bool IsOutOfBounds(Position position)
+    {
+        return (position.X < 0 || position.Y < 0 || position.X >= Width || position.Y >= Height);
+    }
+    
     // Returns true if given position has caused a robot to be lost already
-    public bool HasLostFromPosition(Position position)
+    public bool HasAlreadyLostFromPosition(Position position)
     {
         return LostPositions.Any(lostPosition => 
             lostPosition.X == position.X && lostPosition.Y == position.Y && lostPosition.Orientation == position.Orientation
